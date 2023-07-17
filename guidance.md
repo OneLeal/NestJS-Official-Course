fdc45b4f02ec29f9ea62a459b314bbfcd35698ed
Momota <15639067881@163.com>
2023-07-17 15:51:01 +0800

    feat: create a service to learn

--- guidance.md
+++ guidance.md
@@ -5,3 +5,6 @@ Official Course For NewCome.(https://www.theitzy.com/official-nestjs/)
 
 -- nest generate controller [name] (create new controller)
 -- nest generate controller [name] --dry-run (to see simulated output)
+
+-- nest generate service [name] (create new service)
+-- nest generate service [name] --dry-run (to see simulated output)
--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,34 +8,37 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get() // @Get('flavors') 表示嵌套路由: coffees/flavors
   findAll(@Query() paginationQuery: { limit: number; offset: number }) {
-    const { limit, offset } = paginationQuery;
-    return `this action returns all coffees, limit: ${limit}, offset: ${offset}`;
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
   }
 
   @Get(':id') // 接收动态参数
   findOne(@Param('id') id: string) {
     // @Param 可以通过传入字符串来访问特定参数属性
-    return `this action returns #${id} coffee`;
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   create(@Body() body: any) {
     // @Body 也能传入一个字符串访问 body 中特定的属性
-    return body;
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body: any) {
-    return `this action updates #${id} coffee`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `this action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
   }
 }
